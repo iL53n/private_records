8342a7ab39abf28e35881d8afeb79f932d58c78c
iL53n <ilsegribko@gmail.com>
2020-09-26 19:40:46 +0300

    add Candidate model

--- app.rb
+++ app.rb
@@ -11,18 +11,18 @@ class Candidate
   include Mongoid::Document
 
   field :uid,        type: String
-  # field :first_name, type: String
-  # field :last_name,  type: String
-  # field :email,      type: String
-  # field :phone,      type: String
+  field :first_name, type: String
+  field :last_name,  type: String
+  field :email,      type: String
+  field :phone,      type: String
 
-  # validate :first_name,
-  #          :last_name,
-  #          :email,
-  #          :phone,
-  #          presence: true
-  #
-  # index({ uid: 1 }, { unique: true, name: 'uid_index' })
+  validates :first_name,
+            :last_name,
+            :email,
+            :phone,
+            presence: true
+
+  index({ uid: 1 }, { unique: true, name: 'uid_index' })
 end
 
 # Endpoints
