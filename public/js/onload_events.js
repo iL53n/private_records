7748cff31613999fb7dfe6d1fcf3e970b8e9ea91
rybalchenko <gravemaggot1@gmail.com>
2020-11-26 18:08:03 +0300

    complete candidate edit refactoring phase

--- public/js/onload_events.js
+++ public/js/onload_events.js
@@ -26,6 +26,9 @@ $(function(){
     contentElement = document.getElementById('language_content')
     if (contentElement){ fill_language_content(contentElement) }
 
+    contentElement = document.getElementById('reccomenders_content')
+    if (contentElement){ fill_reccomenders_content(contentElement) }
+
     // Delete button
     $(document).on("click", ".delete", function(){
         $(this).parents("tr").remove();
