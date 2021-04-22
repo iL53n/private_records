8ab81dd9dfc35b4974ead015b02021d28d9503f9
rybalchenko <gravemaggot1@gmail.com>
2021-04-22 16:04:41 +0300

    i18n gem installed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
+require 'sinatra/support/i18nsupport'
 
 # App controller Class
 class ApplicationController < Sinatra::Base
   register Sinatra::Namespace
+  register Sinatra::I18nSupport
 
+  load_locales './config/locales'
+  set :default_locale, 'ru' # Optional; defaults to 'en'
   enable :inline_templates
+
   # enable :sessions
   use Rack::Session::Cookie, {
     key: 'rack.session',
